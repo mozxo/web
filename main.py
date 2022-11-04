cc23c2ba317abc437af5bbe38d0679b570df16a9
Yordi Mamo <94254616+Yoxmo@users.noreply.github.com>
2022-11-03 20:19:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import requests, os, shutil , time , subprocess, glob
 from bs4 import BeautifulSoup
 
-subprocess.Popen(('python3', 'app.py')) 
+name = input('[?] Enter name: ')
 
-while True:
-  
-  result = subprocess.check_output("cd C4C-Yoxmo-LS && git pull", shell=True)
-  
-  if str(result.decode()).strip('\n') != "Already up to date.":
-    files = glob.glob("C4C-Yoxmo-LS/*")
-    first = f"""import flask , os
+if len(glob.glob(f"C4C-{name}-LS/*")) <= 1:
+  os.system(f'git clone https://github.com/{name}/C4C-{name}-LS.git')
+else:
+  pass
+
+def addroutes():
+  files = glob.glob(f"C4C-{name}-LS/*")
+  first = f"""import flask , os
 from flask import Flask, render_template , redirect , request
 
-template_dir = os.path.abspath('C4C-Yoxmo-LS')
-static_dir = os.path.abspath('C4C-Yoxmo-LS/assets')
+template_dir = os.path.abspath('C4C-{name}-LS')
+static_dir = os.path.abspath('C4C-{name}-LS/assets')
 
 app = Flask(__name__, template_folder=template_dir , static_folder=static_dir)
 
@@ -24,29 +25,46 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 def index():
   return render_template('index.html')
 """ 
+  
+  open('app.py', 'w+').write(first)
+  
+  for item in files:
+    if item == f"C4C-{name}-LS/index.html":
+      pass
+    else:
+      if item.endswith('html'):
+        item = item.strip(f'C4C-{name}-LS')
+        item = item.strip('.html')
+        item = item.strip('/')
     
-    open('route.py', 'w+').write(first)
-    
-    for item in files:
-      if item == "C4C-Yoxmo-LS/index.html":
-        pass
-      else:
-        if item.endswith('html'):
-          item = item.strip('C4C-Yoxmo-LS')
-          item = item.strip('.html')
-          item = item.strip('/')
-      
-          cleanedr = f"""\n #==== New Route ====#
+        cleanedr = f"""\n #==== New Route ====#
 @app.route('/{item}')
 def {item}():
   return render_template('{item}.html')
 """
-          open('route.py', 'a+').write(cleanedr)
-        pass
+        open('app.py', 'a+').write(cleanedr)
+      pass
+  
+  print('[*] Added all routes...')
+
+  last = f"""\nif '__main__' == __name__:
+    app.run(host='0.0.0.0', port='5000', debug=True)
+  """ 
     
-    print('[*] Added all routes...')
+  open('app.py', 'a+').write(last)
+
+
+
+addroutes()
+
+subprocess.Popen(('python3', 'app.py')) 
+
+while True:
   
+  result = subprocess.check_output(f"cd C4C-{name}-LS && git pull", shell=True)
+  
+  if str(result.decode()).strip('\n') != "Already up to date.":
+    addroutes()
   else:
-  
     print('[*] No change...')
     time.sleep(1)
